8a58237756b9f711f13a173a52052d30cccfa958
Felippe Santana <f-elipp-3@hotmail.com>
2025-05-09 20:51:04 +0100

    updated global options

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 -- ~/.config/nvim/init.lua
 
+local opt = vim.opt
+
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
@@ -10,15 +12,22 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Basic settings
-vim.opt.number = true             -- Show line numbers
-vim.opt.relativenumber = true    -- Show relative line numbers (e.g., 1, 2, 3 above/below cursor) vim.opt.mouse = "a"              -- Enable mouse support
-vim.opt.clipboard = "unnamedplus"-- Use system clipboard for copy/paste
-vim.opt.expandtab = false         -- Convert tabs to spaces
-vim.opt.shiftwidth = 4           -- Indent size when autoindenting
-vim.opt.tabstop = 4              -- Number of spaces per tab
-vim.opt.termguicolors = true     -- Enable true color support
-vim.opt.list = true
-vim.opt.listchars = {
+opt.number = true             -- Show line numbers
+opt.relativenumber = true    -- Show relative line numbers (e.g., 1, 2, 3 above/below cursor)
+opt.mouse = "a"              -- Enable mouse support
+opt.clipboard = "unnamedplus"-- Use system clipboard for copy/paste
+opt.expandtab = false         -- Use tabs instead of spaces
+opt.tabstop = 4              -- Number of spaces a tab represents
+opt.shiftwidth = 4           -- Size of an indent
+opt.softtabstop = 4          -- How many spaces a tab insert
+opt.termguicolors = true     -- Enable true color support
+opt.list = true              -- Shows invisible characters like tabs, spaces,line breaks etc...
+
+opt.autoindent = true        -- Copy the indentation from previous line
+-- vim.opt.smartindent = true       -- (use with autoindent) adds basics C rules to autoindent
+opt.cindent = true             --(dont use with smartindent) more advanced C sysntax indentation, smarter than smartindent. parses more deeply.
+
+opt.listchars = {
   tab = '➡ ',
   space = '·',
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,36 @@
+-- Define the function first
+local function visual_selection_count()
+  local mode = vim.fn.mode()
+  if mode ~= 'v' and mode ~= 'V' and mode ~= '' then
+    return ''
+  end
+
+  local start_pos = vim.fn.getpos("v")
+  local end_pos = vim.fn.getpos(".")
+  local start_line, start_col = start_pos[2], start_pos[3]
+  local end_line, end_col = end_pos[2], end_pos[3]
+
+  if start_line > end_line or (start_line == end_line and start_col > end_col) then
+    start_line, end_line = end_line, start_line
+    start_col, end_col = end_col, start_col
+  end
+
+  local lines = vim.fn.getline(start_line, end_line)
+  if #lines == 0 then return '0 chars' end
+
+  if #lines == 1 then
+    return tostring(end_col - start_col + 1) .. ' chars'
+  end
+
+  local count = #lines[1] - start_col + 1
+  for i = 2, #lines - 1 do
+    count = count + #lines[i]
+  end
+  count = count + end_col
+
+  return tostring(count) .. ' chars'
+end
+
 return {
     {
         'nvim-lualine/lualine.nvim',
@@ -26,7 +59,7 @@ return {
               sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
+                lualine_c = {'filename', visual_selection_count},
                 lualine_x = {'encoding', 'fileformat', 'filetype'},
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
